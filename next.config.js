7433ab33d1ff2b4dfd4ac3990f6d3ace197395fc
Ysato <y.sugar0402@gmail.com>
2026-01-12 05:17:30 +0900

    for deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
